b9e7c5a97436ff13f60b42e28cc757e80e0dca8e
hckang2000 <hckang2000@naver.com>
2025-04-15 19:14:46 +0900

    Fix: Render 배포를 위한 build.sh 파일 수정

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # exit on error
 set -o errexit
 
+# Python 및 pip 업그레이드
+python -m pip install --upgrade pip
+
+# 의존성 설치
 pip install -r requirements.txt
 
-python mysite/manage.py collectstatic --no-input
-python mysite/manage.py migrate 
+# 정적 파일 수집 및 데이터베이스 마이그레이션
+cd mysite
+python manage.py collectstatic --no-input
+python manage.py migrate 
